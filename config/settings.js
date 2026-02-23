340838b5e13a8a4360a3b453ab1fe523061c22e3
bilalbbh16-maker <bilalbbh16@gmail.com>
2026-02-23 07:34:07 +0100

    Create settings.js

---
+++ config/settings.js
@@ -0,0 +1,8 @@
+Enterconst SETTINGS = {
+    bannerAdCode: "<!-- Add your banner ad code here -->",
+    videoAdCode: "<!-- Add video ad code here -->",
+    smartLinkCode: "<!-- Add Smart Link code here -->",
+    faucetPayApiKey: "YOUR_FAUCETPAY_API_KEY",
+    nowPaymentApiKey: "YOUR_NOWPAYMENT_API_KEY",
+    minWithdraw: 0.5
+};
